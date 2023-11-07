1066995d2a83ec4820884bdcd32bc17416ef1d6f
Lyn <143884194+its-Lyn@users.noreply.github.com>
2023-11-07 23:05:34 +0200

    change tbody to river_body

--- Scripts/Entities/FurnitureBase.gd
+++ Scripts/Entities/FurnitureBase.gd
@@ -2,20 +2,20 @@ extends Area2D
 
 @export var dialogue_path = ""
 var _river_in := false
-var tbody
+var river_body
 
 func _physics_process(delta):
-	if _river_in and Input.is_action_just_pressed("Interact") and tbody != null:
-		tbody.interact(dialogue_path)
+	if _river_in and Input.is_action_just_pressed("Interact") and river_body != null:
+		river_body.interact(dialogue_path)
 
 
 func _on_body_entered(body):
 	if body.name == "River":
 		_river_in = true
-		tbody = body
+		river_body = body
 
 
 func _on_body_exited(body):
 	if body.name == "River":
 		_river_in = false
-		tbody = null
+		river_body = null
